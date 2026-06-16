2b92f70ee231a78f2f97c54e1af076dc23932485
Kishore <kishore.m@lyncspace.com>
2026-06-16 17:43:14 +0000

    feat: add Generate Cards dialog to Coupon Card list view

--- coupon_system/public/js/coupon_card_list.js
+++ coupon_system/public/js/coupon_card_list.js
@@ -12,11 +12,77 @@ frappe.listview_settings["Coupon Card"] = {
 			window.open(`/print_cards?${params}`, "_blank");
 		});
 
-		// Always-visible button in the page header
+		// Always-visible buttons in the page header
 		listview.page.add_inner_button(__("Print Filtered Cards"), () => {
 			const filters = listview.get_filters_for_args();
 			const params = new URLSearchParams({ filters: JSON.stringify(filters) });
 			window.open(`/print_cards?${params}`, "_blank");
+		});
+
+		listview.page.add_inner_button(__("Generate Cards"), () => {
+			const dialog = new frappe.ui.Dialog({
+				title: __("Generate Coupon Cards"),
+				fields: [
+					{
+						fieldname: "quantity",
+						label: __("Quantity"),
+						fieldtype: "Int",
+						reqd: 1,
+					},
+					{
+						fieldname: "item_code",
+						label: __("Item Code"),
+						fieldtype: "Link",
+						options: "Item",
+						reqd: 1,
+					},
+					{
+						fieldname: "points_value",
+						label: __("Points Value"),
+						fieldtype: "Float",
+						reqd: 1,
+					},
+					{
+						fieldname: "expiry_date",
+						label: __("Expiry Date"),
+						fieldtype: "Date",
+						reqd: 1,
+					},
+					{ fieldtype: "Column Break" },
+					{
+						fieldname: "batch_no",
+						label: __("Batch No"),
+						fieldtype: "Data",
+					},
+					{
+						fieldname: "work_order",
+						label: __("Work Order"),
+						fieldtype: "Data",
+					},
+				],
+				primary_action_label: __("Generate"),
+				primary_action(values) {
+					dialog.disable_primary_action();
+					frappe.call({
+						method: "coupon_system.api.generate_cards",
+						args: values,
+						callback(r) {
+							dialog.enable_primary_action();
+							if (r.message?.success) {
+								dialog.hide();
+								frappe.show_alert({
+									message: __("{0} cards generated.", [r.message.count]),
+									indicator: "green",
+								});
+								listview.refresh();
+							} else {
+								frappe.msgprint(r.message?.error || __("Generation failed."));
+							}
+						},
+					});
+				},
+			});
+			dialog.show();
 		});
 	},
 };
